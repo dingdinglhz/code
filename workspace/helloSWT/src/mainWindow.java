b8a2085f11085486b4b049b6faf34b70095ac94b
dingdinglhz <dingdinglhz@gmail.com>
2013-08-26 22:17:04 -0400

    add old workspace codes
    add old workspace codes

---
+++ workspace/helloSWT/src/mainWindow.java
@@ -0,0 +1,31 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+
+
+public class mainWindow {
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = Display.getDefault();
+		Shell shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Label lblHelloWorld = new Label(shell, SWT.NONE);
+		lblHelloWorld.setBounds(83, 93, 122, 82);
+		lblHelloWorld.setText("hello world");
+
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+}
