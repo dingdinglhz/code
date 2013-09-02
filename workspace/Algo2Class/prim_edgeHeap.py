bc9a77b4de0aaca7387caff4028a7281005ce813
dingdinglhz <dingdinglhz@gmail.com>
2013-09-01 22:04:59 -0400

    prim edgeHeap version complete

---
+++ workspace/Algo2Class/prim_edgeHeap.py
@@ -0,0 +1,68 @@
+
+import sys, os
+from heapq import *
+graph = {}
+tree=set()
+treeEdges=set()
+heap=[]
+firstVertice=1
+treeCost=0;
+class edge:
+    def __init__(self,x=0,y=0,cost=0):
+        self.x=x
+        self.y=y
+        self.cost=cost
+    def __lt__(self,y):
+        return self.cost<y.cost
+    def __rt__(self,y):
+        return self.cost>y.cost
+    def __str__(self):
+        return "%d - %d cost:%d "%(self.x,self.y,self.cost)
+if __name__ == '__main__':
+    sys.stdin=open("prim.txt", encoding='utf-8')
+    (n, m) =(int(tmp) for tmp in input().split())
+    for i in range(1,n+1):
+        graph.setdefault(i,{})
+    #print(graph)
+    for i in range(0,m):
+        (x,y,cost) =(int(tmp) for tmp in input().split())
+        graph[x][y]=cost
+        graph[y][x]=cost
+    #print(graph)
+    tree.add(firstVertice)
+    for v in graph[firstVertice]:
+        heappush(heap, edge(firstVertice,v,graph[firstVertice][v]) )
+    '''htmp=heap.copy()
+    print(len(heap))
+    for edgei in range(len(htmp)):
+        print(heappop(htmp))'''
+    while len(tree)<n:
+        tmp=heappop(heap)
+        while ((tmp.x in tree)^(tmp.y in tree))==False:
+            tmp=heappop(heap)
+        tree.add(tmp.y)
+        treeEdges.add(tmp)
+        treeCost+=tmp.cost
+        for v in graph[tmp.y]:
+            heappush(heap, edge(tmp.y,v,graph[tmp.y][v]) )
+        
+    print(treeCost)
+    
+'''
+small scale data
+4 5
+2 1 1
+4 3 5
+4 1 4
+2 3 2
+1 3 3
+out:7
+5 6
+1 2 6
+1 3 3
+3 4 12
+1 4 -14
+2 4 8
+4 5 -5
+out:-10
+'''
--- workspace/Algo2Class/week1problem1_2.py
+++ workspace/Algo2Class/week1problem1_2.py
@@ -3,7 +3,7 @@ Created on 2013-9-1
 
 @author: linhz
 '''
-import sys,os
+import sys, os
 class RedirectStdinTo:
     def __init__(self, in_new):
         self.in_new = in_new
@@ -16,35 +16,45 @@ class RedirectStdinTo:
         sys.stdin = self.in_old
         
 class job:
-    def __init__(self,weight=0,length=0):
-        self.weight=weight
-        self.length=length
-        self.key=length/weight
+    def __init__(self, weight, length):
+        self.weight = weight
+        self.length = length
+        self.key = length / weight
     def __str__(self):
-        #return "weight: "+str(self.weight)+" length: "+str(self.length)+" key: "+str(self.key)
-        return "weight: %d length: %d key: %d"%(self.weight,self.length,self.key)
-    def __lt__(self,y):
-        if self.key==y.key:
-            return self.weight>y.weight
-        return self.key<y.key
+        # return "weight: "+str(self.weight)+" length: "+str(self.length)+" key: "+str(self.key)
+        return "weight: %d length: %d key: %d" % (self.weight, self.length, self.key)
+    def __lt__(self, y):
+        if self.key == y.key:
+            return self.weight > y.weight
+        return self.key < y.key
 
 
 if __name__ == '__main__':
     with open("jobs.txt", encoding='utf-8') as inFile, RedirectStdinTo(inFile):
-        num=int(input())
-        jobList=[]
-        for i in range(0,num):
-            inputTmp=input().split()
-            jobi=job(int(inputTmp[0]),int(inputTmp[1]))
+        num = int(input())
+        jobList = []
+        for i in range(0, num):
+            inputTmp = input().split()
+            jobi = job(int(inputTmp[0]), int(inputTmp[1]))
             jobList.append(jobi)
         jobList.sort()
-        time=0
-        result=0
+        time = 0
+        result = 0
         for i in jobList:
-            #print(i)
-            time+=i.length
-            result+=i.weight*time;
-            #print("time %d result %d"%(time,result))
-            #os.system("pause")
+            # print(i)
+            time += i.length
+            result += i.weight * time;
+            # print("time %d result %d"%(time,result))
+            # os.system("pause")
         print(result)
         
+'''
+small scale data
+6
+30 90
+10 30
+20 40
+30 50
+20 60
+10 50 
+'''
